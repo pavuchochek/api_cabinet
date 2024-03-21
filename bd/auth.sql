67b108c74008dc700371339a08157a928c1a82d0
Sofia Gribanova <grisoffa@gmail.com>
2024-03-21 10:10:48 +0100

    Update auth.sql

--- bd/auth.sql
+++ bd/auth.sql
@@ -5,11 +5,3 @@ CREATE TABLE user_auth_v1(
 );
 INSERT INTO user_auth_v1 VALUES('secretaire1', '$2y$10$O2BnJxQq7wpub5.PIIJTnebUUWvO7nuNG5JpXOuFGbqAKSk6xEWZq');
 
-CREATE TABLE user_auth_v2(
-   login VARCHAR(50),
-   mdp VARCHAR(50) NOT NULL,
-   id_auth VARCHAR(50) NOT NULL,
-   role VARCHAR(50),
-   PRIMARY KEY(login),
-   UNIQUE(id_auth)
-);
